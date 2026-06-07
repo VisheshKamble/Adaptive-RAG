f97e9ee70879631e00479e18148b2eaf545c7ce2
Vishesh Kamble <visheshy2k17@gmail.com>
2026-06-07 15:22:17 +0530

    Update ui/src/App.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar'; // ← add
 import LandingPage from './pages/LandingPage';
 import AppPage from './pages/AppPage';
 
 function App() {
   return (
     <Router>
+
+      <Navbar /> {/* ← render navbar */}
+
       <Routes>
-        {/* The initial screen visitors see */}
         <Route path="/" element={<LandingPage />} />
-        
-        {/* Your Adaptive-RAG system dashboard */}
         <Route path="/app" element={<AppPage />} />
       </Routes>
+
     </Router>
   );
 }
